cd13d5e72d94f91ff0a328fecd3f169accb060ea
Juan <juandimatemiguel@gmail.com>
2025-02-08 20:16:52 -0500

    controllers

--- src/main/java/com/trabajo1/ApiNombres/controller/BdController.java
+++ src/main/java/com/trabajo1/ApiNombres/controller/BdController.java
@@ -1,7 +1,10 @@
 package com.trabajo1.ApiNombres.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +18,16 @@ public class BdController {
     @Autowired
     private NamesRepositories namesRepositories;
 
-    @PostMapping("/guardar")
-    public ResponseEntity<Names> guardar(@RequestParam String namein){
+    @PostMapping("/save")
+    public ResponseEntity<Names> save(@RequestParam String namein){
         Names name = new Names(namein);
         return ResponseEntity.ok().body(namesRepositories.save(name));
-        
-       
     }
+
+    @GetMapping("/getNames")
+    public List<String> getNames(){
+        return namesRepositories.findAllNames();
+    }
+
+    
 }
--- src/main/java/com/trabajo1/ApiNombres/repositories/NamesRepositories.java
+++ src/main/java/com/trabajo1/ApiNombres/repositories/NamesRepositories.java
@@ -1,9 +1,14 @@
 package com.trabajo1.ApiNombres.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.trabajo1.ApiNombres.models.Names;
 
 public interface NamesRepositories extends JpaRepository<Names,Long> {
     
+    @Query("SELECT u.name FROM Names u")
+    List<String> findAllNames();
 }
