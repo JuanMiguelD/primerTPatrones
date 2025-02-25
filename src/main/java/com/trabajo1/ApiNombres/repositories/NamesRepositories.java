8868f171c75c21ce909653f6aada2148f32fef95
Andrey10co <124622713+Andrey10co@users.noreply.github.com>
2025-02-25 17:42:26 -0500

    Funtion delete name

--- src/main/java/com/trabajo1/ApiNombres/repositories/NamesRepositories.java
+++ src/main/java/com/trabajo1/ApiNombres/repositories/NamesRepositories.java
@@ -3,7 +3,9 @@ package com.trabajo1.ApiNombres.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.trabajo1.ApiNombres.models.Names;
 
@@ -11,4 +13,10 @@ public interface NamesRepositories extends JpaRepository<Names,Long> {
     
     @Query("SELECT u.name FROM Names u")
     List<String> findAllNames();
+
+    @Modifying
+    @Query("DELETE FROM Nombre n WHERE n.nombre = :name")
+    void deleteByName(@Param("name") String name);
+    //funtion for delete a name by name
+
 }
